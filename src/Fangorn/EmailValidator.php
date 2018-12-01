95274631158363b0ec416f7f09d84a6be70457a7
Burenkov Sergey <burenkovsergey@mail.ru>
2018-12-01 12:58:08 +0300

    Убрал забытый todo; добавил .gitignore

--- src/Fangorn/EmailValidator.php
+++ src/Fangorn/EmailValidator.php
@@ -7,8 +7,6 @@ class EmailValidator {
     public function isValid(string $adderess): bool {
         $username = '([a-z_\'0-9а-яё]+|\"[a-z_\'\,0-9а-яё]+\")';
         $host     = '(([a-z\-а-яё]+\.)+([a-zа-яё]+)|localhost)';
-
-        // TODO: Может быть любой уровень домена test@a.b.c.d.e.f.g.ru
         $pattern = '/^' . $username . '@' . $host . '$/iu';
         return preg_match($pattern, $adderess);
     }
